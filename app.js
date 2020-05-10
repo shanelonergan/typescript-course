f8ef94ed2f9c29370ad74b008bca0f880592f7cf
shanelonergan <sptlonergan@gmail.com>
2020-05-10 11:04:39 -0400

    tuples

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-function add(n1, n2, showResult) {
-    if (showResult) {
-        console.log(n1 + n2);
-    }
-    else {
-        return n1 + n2;
-    }
+var person = {
+    name: 'Shane',
+    age: 30,
+    hobbies: ['Sports', 'Cooking'],
+    role: [2, 'author']
+};
+var favoriteActivities;
+favoriteActivities = ['Sports'];
+console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase()); // TS knows all values will be strings
 }
-var number1 = 5;
-var number2 = 2.8;
-var printResult = true;
-add(number1, number2, printResult);
---
+++ app.ts
@@ -0,0 +1,20 @@
+const person: {
+    name: string;
+    age: number;
+    hobbies: string[];
+    role: [number, string];
+} = {
+	name: 'Shane',
+	age: 30,
+    hobbies: ['Sports', 'Cooking'],
+    role: [2, 'author']
+}
+
+let favoriteActivities: string[]
+favoriteActivities = ['Sports']
+
+console.log(person.name)
+
+for (const hobby of person.hobbies) {
+    console.log(hobby.toUpperCase()) // TS knows all values will be strings
+}
