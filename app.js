f3a57751aaf1c7419812f7126845c0a33d9428d3
shanelonergan <sptlonergan@gmail.com>
2020-05-11 12:36:37 -0400

    literal types

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-var person = {
-    name: 'Shane',
-    age: 30,
-    hobbies: ['Sports', 'Cooking'],
-    role: [2, 'author']
-};
-var favoriteActivities;
-favoriteActivities = ['Sports'];
-console.log(person.name);
-for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
-    var hobby = _a[_i];
-    console.log(hobby.toUpperCase()); // TS knows all values will be strings
+// Union types
+function combine(input1, input2, resultConversion) {
+    var result;
+    if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
+        result = +input1 + +input2;
+    }
+    else {
+        result = input1.toString() + input2.toString();
+    }
+    return result;
 }
+var combinedAges = combine(30, 26, 'as-number');
+console.log(combinedAges);
+var combinedNames = combine('Max', 'Anna', 'as-string');
+console.log(combinedNames);
--- app.ts
+++ app.ts
@@ -1,12 +1,21 @@
 // Union types
-function combine(input1: number | string. input: number | string) {
+function combine(
+    input1: number | string,
+    input2: number | string,
+    resultConversion: 'as-number' | 'as-text' // literal types, helps prevent errors due to types
+) {
+	let result
+	if ((typeof input1 === 'number' && typeof input2 === 'number') || resultConversion === 'as-number') {
+		result = +input1 + +input2
+	} else {
+		result = input1.toString() + input2.toString()
+	}
 
-    let result
-    if (typeof input 1 === 'number' && typeof input2 === 'number') {
-        result = input1 + input2
-    } else {
-        result = input1.toString() + input2.toString()
-    }
-
-    return result
+	return result
 }
+
+const combinedAges = combine(30, 26, 'as-number')
+console.log(combinedAges)
+
+const combinedNames = combine('Max', 'Anna', 'as-string')
+console.log(combinedNames)
